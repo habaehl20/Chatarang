2ed19d97ca8d599a9cb9baf5b65bd83fa5a25727
Hunter Baehl <habaehl20@wabash.edu>
2018-06-12 09:51:35 -0400

    style main

--- src/Main.js
+++ src/Main.js
@@ -3,19 +3,26 @@ import React, { Component } from 'react'
 import './App'
 import Main from './Main'
 import Sidebar from './Sidebar'
+import Chat from './Chat'
 
-class App extends Component {
+class Main extends Component {
     render() {
         return(
-            <div className='App'>
-            
-                
+            <div className='App' style={styles}>
+                <Sidebar />  
+                <Chat />           
 
             </div>
         )
 
     }
         
+const styles = {
+display: 'flex',
+alignItems: 'stretch',
+height: '100vh',
 }
 
-export default App
+}
+
+export default Main
