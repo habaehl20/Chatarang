12436aa897fbd32c8cecc795f70de6dc1e10ea9d
Hunter Baehl <habaehl20@wabash.edu>
2018-06-12 09:36:27 -0400

    add sidebar and chat components

---
+++ src/Chat.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Chat extends Component {
+    render() {
+        return(
+            <div className='Chat'>
+               chat
+                
+            </div>
+        )
+
+    }
+        
+}
+
+export default Chat
---
+++ src/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import './App'
+import Main from './Main'
+import Sidebar from './Sidebar'
+
+class App extends Component {
+    render() {
+        return(
+            <div className='App'>
+            
+                
+
+            </div>
+        )
+
+    }
+        
+}
+
+export default App
