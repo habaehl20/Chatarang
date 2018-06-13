d504ed2ee5155c46b42cf93b32e5c98d83fc22ea
Hunter Baehl <habaehl20@wabash.edu>
2018-06-13 11:35:18 -0400

    add re-base

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import './App.css'
 import Main from './Main'
-import SignIn fomr './SignIn'
+import SignIn from './SignIn'
 
 class App extends Component {
   constructor(){
